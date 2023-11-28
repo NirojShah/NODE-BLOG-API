dbe2618f29efd9bdfdcc8591cadb0d51f72a86b2
Niraj Shah <Nirajshah950@gmail.com>
2023-11-28 17:38:26 +0530

    custom error handling

--- app.js
+++ app.js
@@ -14,4 +14,33 @@ app.use("/app/v1/user",userRouter)
 // app.use("/app/v1/profile",auth,blogRoute) ==> we can pass here also
 app.use("/app/v1/blogs",blogRouter)
 
+
+
+// Exept above route any other route will throw error...
+
+app.all("*",(req,res,next)=>{
+    let error = new Error("PAGE NOT FOUND")
+    error.statusCode=404,
+    error.status = "FAILED"
+
+    next(error)  // used to throw errro..
+
+})
+
+
+// GLOBAL ERROR HANDLER
+
+app.use((err,req,res,next)=>{
+    err.statusCode=err.statusCode || 500
+    err.status=err.status || "ERROR !!!"
+    err.message=err.message || "Something went wrong"
+
+    res.status(err.statusCode).json({
+        status:err.status,
+        data:{
+            msg:err.message
+        }
+    })
+})
+
 module.exports = app
