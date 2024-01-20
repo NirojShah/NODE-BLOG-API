47fdc2e7af149f987f16a0ee6d1a42a8faac97c4
Niraj Shah <Nirajshah950@gmail.com>
2024-01-20 14:06:49 +0530

    created google model and route for google

---
+++ google/passport.js
@@ -0,0 +1,35 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const passport = require("passport")
+const googleModel = require("../models/googleModel")
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async function (id, done) {
+    let user = await googleModel.findById(id)
+    done(null, user)
+})
+
+passport.use(new GoogleStrategy({
+    callbackURL: 'http://localhost:5173/home',
+    clientID: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+}, async (request, acessToken, refreshToken, profile, done) => {
+    let currentUser = await googleModel.findOne({
+        googleId: profile.id
+    })
+    if (currentUser) {
+        console.log(`currentuser:`, currentUser);
+        done(null, currentUser)
+    } else {
+        let newUser = await googleModel.create({
+            username: profile.displayName,
+            googleId: profile.id
+        })
+        console.log(newUser);
+        done(null, newUser)
+    }
+}))
+
+module.exports = passport
